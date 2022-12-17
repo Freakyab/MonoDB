8bd15e0e978c6922acf28d97254538f728aeab38
Freakyab <aryanbhisikar1@gmail.com>
2022-12-18 01:22:21 +0530

    change password correction

--- PasswordManager.py
+++ PasswordManager.py
@@ -21,14 +21,15 @@ def fetch_data(website_name):
     for result in results:
         final_keys=list(result.keys())
         final_values=list(result.values())
-
         if(website_name in final_values):
             position_of_website_name = final_values.index(website_name)
             passwords =  final_values[position_of_website_name+1]
             username = input("Enter the username: ")
+
             if username in passwords:
+
                 position_of_username = passwords.index(username)
-                return passwords[position_of_username+1],position_of_username,username
+                return passwords[position_of_username+1],position_of_username,username,passwords
             else:
                 return False
     else:
@@ -62,29 +63,22 @@ def print_db():
         print(i)
 
 # to change the password
-def change_password(password,position_of_username,username):
+def change_password(password,passwords,username):
     
     new_password = input("Enter the new password: ")
     check = input("Do you want to save the password? (y/n): ")
     if check == 'y':
-        new_main_dict={'account_data':[username,new_password]}
+        position_of_password = passwords.index(username)
+        passwords[position_of_password+1] = new_password
+        new_main_dict={'account_data':passwords}
         client_name.collection.update_one({'website_name':website_name},{'$set':new_main_dict})
     else:
         print("Password not saved")
 
 # to delete the data
 
-def delete_data(website_name,password,position_of_username,username):
-    Tofind = {'website_name':website_name}
-    results = client_name.collection.find(Tofind)
-
-    for result in results:
-        final_keys=list(result.keys())
-        final_values=list(result.values())
-
-        if(website_name in final_values):
-            position_of_website_name = final_values.index(website_name)
-            passwords =  final_values[position_of_website_name+1]
+def delete_data(website_name,password,passwords,username):
+   
     passwords.remove(username)
     passwords.remove(password)
     new_main_dict={'account_data':passwords}
@@ -131,16 +125,16 @@ if main_key == '0112':
         insert_data()
     elif action == 2:  
         website_name = input("Enter the name of website: ")
-        result,position_of_username,username = fetch_data(website_name)
+        result,position_of_username,username ,passwords= fetch_data(website_name)
         if result == False:
             print("Invalid")
         else:
                
             action1 = int(input("Enter the action you want to perform:\n1.change_password\n2.delete_data\n3.print_password\n4.print_db\n5.Insert_data\nEnter: "))
             if action1 == 1:
-                change_password(result,position_of_username,username)
+                change_password(result,passwords,username)
             elif action1 == 2:
-                delete_data(website_name,result, position_of_username, username)
+                delete_data(website_name,result, passwords, username)
                 print_db()
             elif action1 == 3:
                 print_password(result, position_of_username, username)
@@ -150,9 +144,5 @@ if main_key == '0112':
                 insert_data_inside_website(website_name)
             else:
                  print("Invalid")
-       
-    # else:
-    #     print("Invalid")
-
 else:
     print("Invalid Password")
